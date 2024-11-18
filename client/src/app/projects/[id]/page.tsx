d874bccffc45864d72899eecd4ae6b8ec390f232
Gaurav <gauravhpanchal2@gmail.com>
2024-11-18 17:59:42 +0530

    init

---
+++ client/src/app/projects/[id]/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import ProjectHeader from "../ProjectHeader";
+import React, { use, useState } from "react";
+import BoardView from "../BoardView";
+import ListView from "../ListView";
+import TimeLineView from "../TimelineView";
+import TableView from "../TableView";
+import ModalNewTask from "../../../components/ModalNewTask";
+
+type Props = {
+  params: any;
+};
+
+const Project = ({ params }: Props) => {
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
+  return (
+    <div>
+      {/* Modal New Task */}
+      <ModalNewTask
+        isOpen={isModalNewTaskOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+        id={id}
+      />
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === "Board" && (
+        <BoardView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "List" && (
+        <ListView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Timeline" && (
+        <TimeLineView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Table" && (
+        <TableView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+    </div>
+  );
+};
+
+export default Project;
