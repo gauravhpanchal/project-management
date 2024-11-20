e994d76adc8c1711b4c3ccfcc31ef755e149b006
Gaurav <gauravhpanchal2@gmail.com>
2024-11-20 14:12:53 +0530

    authentication and authorization added

--- client/src/app/priority/reusablePriorityPage/index.tsx
+++ client/src/app/priority/reusablePriorityPage/index.tsx
@@ -5,7 +5,12 @@ import Header from "@/components/Header";
 import ModalNewTask from "@/components/ModalNewTask";
 import TaskCard from "@/components/TaskCard";
 import { dataGridClassNames, dataGridSxStyles } from "@/lib/utils";
-import { Priority, Task, useGetTasksByUserQuery } from "@/state/api";
+import {
+  Priority,
+  Task,
+  useGetAuthUserQuery,
+  useGetTasksByUserQuery,
+} from "@/state/api";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React, { useState } from "react";
 
@@ -14,65 +19,66 @@ type Props = {
 };
 
 const columns: GridColDef[] = [
-    {
-      field: "title",
-      headerName: "Title",
-      width: 100,
-    },
-    {
-      field: "description",
-      headerName: "Description",
-      width: 200,
-    },
-    {
-      field: "status",
-      headerName: "Status",
-      width: 130,
-      renderCell: (params) => (
-        <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-          {params.value}
-        </span>
-      ),
-    },
-    {
-      field: "priority",
-      headerName: "Priority",
-      width: 75,
-    },
-    {
-      field: "tags",
-      headerName: "Tags",
-      width: 130,
-    },
-    {
-      field: "startDate",
-      headerName: "Start Date",
-      width: 130,
-    },
-    {
-      field: "dueDate",
-      headerName: "Due Date",
-      width: 130,
-    },
-    {
-      field: "author",
-      headerName: "Author",
-      width: 150,
-      renderCell: (params) => params.value.username || "Unknown",
-    },
-    {
-      field: "assignee",
-      headerName: "Assignee",
-      width: 150,
-      renderCell: (params) => params.value.username || "Unassigned",
-    },
-  ];
+  {
+    field: "title",
+    headerName: "Title",
+    width: 100,
+  },
+  {
+    field: "description",
+    headerName: "Description",
+    width: 200,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 130,
+    renderCell: (params) => (
+      <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+        {params.value}
+      </span>
+    ),
+  },
+  {
+    field: "priority",
+    headerName: "Priority",
+    width: 75,
+  },
+  {
+    field: "tags",
+    headerName: "Tags",
+    width: 130,
+  },
+  {
+    field: "startDate",
+    headerName: "Start Date",
+    width: 130,
+  },
+  {
+    field: "dueDate",
+    headerName: "Due Date",
+    width: 130,
+  },
+  {
+    field: "author",
+    headerName: "Author",
+    width: 150,
+    renderCell: (params) => params.value.username || "Unknown",
+  },
+  {
+    field: "assignee",
+    headerName: "Assignee",
+    width: 150,
+    renderCell: (params) => params.value.username || "Unassigned",
+  },
+];
 
 const ReusablePriorityPage = ({ priority }: Props) => {
   const [view, setView] = useState("list");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
-  const userId = 1;
+  const { data: currentUser } = useGetAuthUserQuery({});
+  const userId = currentUser?.userDetails?.userId ?? null;
   const {
     data: tasks,
     isLoading,
--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,16 +1,30 @@
 import React from "react";
-import { MenuIcon, MoonIcon, Search, Settings, SunIcon } from "lucide-react";
+import { Menu, Moon, Search, Settings, Sun, User } from "lucide-react";
 import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { useGetAuthUserQuery } from "@/state/api";
+import { signOut } from "aws-amplify/auth";
+import Image from "next/image";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
-
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed,
   );
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const { data: currentUser } = useGetAuthUserQuery({});
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
+  };
+
+  if (!currentUser) return null;
+  const currentUserDetails = currentUser?.userDetails;
 
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
@@ -20,7 +34,7 @@ const Navbar = () => {
           <button
             onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
           >
-            <MenuIcon className="h-8 w-7 dark:text-white" />
+            <Menu className="h-8 w-8 dark:text-white" />
           </button>
         )}
         <div className="relative flex h-min w-[200px]">
@@ -32,33 +46,58 @@ const Navbar = () => {
           />
         </div>
       </div>
+
       {/* Icons */}
       <div className="flex items-center">
         <button
           onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
           className={
             isDarkMode
-              ? "rounded p-2 dark:hover:bg-gray-700"
-              : "rounded p-2 hover:bg-gray-100"
+              ? `rounded p-2 dark:hover:bg-gray-700`
+              : `rounded p-2 hover:bg-gray-100`
           }
         >
           {isDarkMode ? (
-            <SunIcon className="h-6 w-6 cursor-pointer dark:text-white" />
+            <Sun className="h-6 w-6 cursor-pointer dark:text-white" />
           ) : (
-            <MoonIcon className="h-6 w-6 cursor-pointer dark:text-white" />
+            <Moon className="h-6 w-6 cursor-pointer dark:text-white" />
           )}
         </button>
         <Link
           href="/settings"
           className={
             isDarkMode
-              ? "h-min w-min rounded p-2 dark:hover:bg-gray-700"
-              : "h-min w-min rounded p-2 hover:bg-gray-100"
+              ? `h-min w-min rounded p-2 dark:hover:bg-gray-700`
+              : `h-min w-min rounded p-2 hover:bg-gray-100`
           }
         >
           <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
         </Link>
-        <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1em] bg-gray-200 md:inline-block"></div>
+        <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
+        <div className="hidden items-center justify-between md:flex">
+          <div className="align-center flex h-9 w-9 justify-center">
+            {!!currentUserDetails?.profilePictureUrl ? (
+              <Image
+                src={`https://projectmanagement-s3-img.s3.ap-south-1.amazonaws.com/${currentUserDetails?.profilePictureUrl}`}
+                alt={currentUserDetails?.username || "User Profile Picture"}
+                width={100}
+                height={50}
+                className="h-full rounded-full object-cover"
+              />
+            ) : (
+              <User className="h-6 w-6 cursor-pointer self-center rounded-full dark:text-white" />
+            )}
+          </div>
+          <span className="mx-3 text-gray-800 dark:text-white">
+            {currentUserDetails?.username}
+          </span>
+          <button
+            className="hidden rounded bg-blue-400 px-4 py-2 text-xs font-bold text-white hover:bg-blue-500 md:block"
+            onClick={handleSignOut}
+          >
+            Sign out
+          </button>
+        </div>
       </div>
     </div>
   );
