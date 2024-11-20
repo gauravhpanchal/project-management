e27f65104de63f3188728ede43d0dff9b414bd29
Gaurav <gauravhpanchal2@gmail.com>
2024-11-20 12:23:25 +0530

    aws cognito config done

---
+++ client/src/app/authProvider.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Authenticator, Placeholder } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
+      userPoolClientId:
+        process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || "",
+    },
+  },
+});
+
+const formFields = {
+  signUp: {
+    username: {
+      order: 1,
+      placeholder: "Choose a username",
+      label: "Username",
+      type: "text",
+      required: true,
+      // inputProps: { required: true },
+    },
+    email: {
+      order: 2,
+      placeholder: "Enter your email address",
+      label: "Email",
+      type: "email",
+      required: true,
+      // inputProps: { type: "email", required: true },
+    },
+    password: {
+      order: 3,
+      placeholder: "Enter your password",
+      label: "Password",
+      type: "password",
+      required: true,
+      // inputProps: { type: "password", required: true },
+    },
+    confirm_password: {
+      order: 4,
+      placeholder: "Confirm your password",
+      label: "Confirm Password",
+      type: "password",
+      required: true,
+      // inputProps: { type: "password", required: true },
+    },
+  },
+};
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="mt-5">
+      <Authenticator formFields={formFields}>
+        {({ user }) =>
+          user ? (
+            <div>{children}</div>
+          ) : (
+            <div>
+              <h1>Please sign in below:</h1>
+            </div>
+          )
+        }
+      </Authenticator>
+    </div>
+  );
+};
+
+export default AuthProvider;
